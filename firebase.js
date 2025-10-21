01f1713d92754c247408b8c1e40972d286ca6770
Shyn <92263282+shyn2910@users.noreply.github.com>
2025-10-21 19:07:31 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,8 +1,11 @@
-// firebase.js (sửa chuẩn)
+// firebase.js
+// Kết nối Firebase cho Caro Online
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
 import { getDatabase, ref, set, get, child, update, onValue, push } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-database.js";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
 
+// Cấu hình Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyAPqd3bW8OGgzAV08CehKo9LwId8jj7YK0",
   authDomain: "shynapi.firebaseapp.com",
@@ -14,10 +17,12 @@ const firebaseConfig = {
   databaseURL: "https://shynapi-default-rtdb.firebaseio.com"
 };
 
+// Khởi tạo
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 const auth = getAuth(app);
 
+// Xuất các hàm dùng trong các file khác
 export {
   app, db, auth, ref, set, get, child, update, onValue, push,
   createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged
